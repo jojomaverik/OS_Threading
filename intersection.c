be9388ccc580af21af638b91fa6dc32b61e9db50
jojomaverik <arda.bizeldodo@gmail.com>
2025-03-19 16:20:17 +0100

    first commit

---
+++ intersection.c
@@ -0,0 +1,123 @@
+/*
+ * Operating Systems (2INC0) Practical Assignment
+ * Threading
+ *
+ * Intersection Part [REPLACE WITH PART NUMBER]
+ * 
+ * STUDENT_NAME_1 (STUDENT_NR_1)
+ * STUDENT_NAME_2 (STUDENT_NR_2)
+ * STUDENT_NAME_3 (STUDENT_NR_3)
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <errno.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#include "arrivals.h"
+#include "intersection_time.h"
+#include "input.h"
+
+// TODO: Global variables: mutexes, data structures, etc...
+
+/* 
+ * curr_car_arrivals[][][]
+ *
+ * A 3D array that stores the arrivals that have occurred
+ * The first two indices determine the entry lane: first index is Side, second index is Direction
+ * curr_arrivals[s][d] returns an array of all arrivals for the entry lane on side s for direction d,
+ *   ordered in the same order as they arrived
+ */
+static Car_Arrival curr_car_arrivals[4][4][20];
+
+/*
+ * car_sem[][]
+ *
+ * A 2D array that defines a semaphore for each entry lane,
+ *   which are used to signal the corresponding traffic light that a car has arrived
+ * The two indices determine the entry lane: first index is Side, second index is Direction
+ */
+static sem_t car_sem[4][4];
+
+/*
+ * supply_cars()
+ *
+ * A function for supplying car arrivals to the intersection
+ * This should be executed by a separate thread
+ */
+static void* supply_cars()
+{
+  int t = 0;
+  int num_curr_arrivals[4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
+
+  // for every arrival in the list
+  for (int i = 0; i < sizeof(input_car_arrivals)/sizeof(Car_Arrival); i++)
+  {
+    // get the next arrival in the list
+    Car_Arrival arrival = input_car_arrivals[i];
+    // wait until this arrival is supposed to arrive
+    sleep(arrival.time - t);
+    t = arrival.time;
+    // store the new arrival in curr_arrivals
+    curr_car_arrivals[arrival.side][arrival.direction][num_curr_arrivals[arrival.side][arrival.direction]] = arrival;
+    num_curr_arrivals[arrival.side][arrival.direction] += 1;
+    // increment the semaphore for the traffic light that the arrival is for
+    sem_post(&car_sem[arrival.side][arrival.direction]);
+  }
+
+  return(0);
+}
+
+
+/*
+ * manage_light(void* arg)
+ *
+ * A function that implements the behaviour of a traffic light
+ */
+static void* manage_light(void* arg)
+{
+  // TODO:
+  // while not all arrivals have been handled, repeatedly:
+  //  - wait for an arrival using the semaphore for this traffic light
+  //  - lock the right mutex(es)
+  //  - make the traffic light turn green
+  //  - sleep for CROSS_TIME seconds
+  //  - make the traffic light turn red
+  //  - unlock the right mutex(es)
+
+  return(0);
+}
+
+
+int main(int argc, char * argv[])
+{
+  // create semaphores to wait/signal for arrivals
+  for (int i = 0; i < 4; i++)
+  {
+    for (int j = 0; j < 4; j++)
+    {
+      sem_init(&car_sem[i][j], 0, 0);
+    }
+  }
+
+  // start the timer
+  start_time();
+  
+  // TODO: create a thread per traffic light that executes manage_light
+
+  // TODO: create a thread that executes supply_cars()
+
+  // TODO: wait for all threads to finish
+
+  // destroy semaphores
+  for (int i = 0; i < 4; i++)
+  {
+    for (int j = 0; j < 4; j++)
+    {
+      sem_destroy(&car_sem[i][j]);
+    }
+  }
+}
