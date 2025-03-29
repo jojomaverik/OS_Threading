8c19c6baffbd07119a14af85d6d07a1791540c7c
jojomaverik <arda.bizeldodo@gmail.com>
2025-03-29 09:01:43 -0700

    new mutexes, new zone list, part 2 done

--- intersection.c
+++ intersection.c
@@ -16,7 +16,10 @@ extern struct timespec begin_time;
 
 static Car_Arrival curr_car_arrivals[4][4][20];
 static sem_t car_sem[4][4];
-pthread_mutex_t intersection_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+// Array of zones, thread
+#define NUM_ZONES 7
+pthread_mutex_t zone_mutex[NUM_ZONES];
 static int processed_count[4][4] = { {0} };
 
 typedef struct {
@@ -54,6 +57,41 @@ static void* supply_cars()
  *
  * Modified to use an absolute deadline equal to begin_time + END_TIME.
  */
+
+// Example lookup: Use -1 as a terminator.
+static int path_zones[4][4][5] = {
+    // NORTH (side 0)
+    {
+        /* LEFT  */ { /* zones for NORTH-LEFT, e.g.: */ -1, -1, -1, -1, -1 },
+        /* STRAIGHT */ { /* zones for NORTH-STRAIGHT, e.g.: */ -1, -1, -1, -1, -1 },
+        /* RIGHT */ { /* zones for NORTH-RIGHT, e.g.: */ -1, -1, -1, -1, -1 },
+        /* UTURN */ { -1, -1, -1, -1, -1 }  
+    },
+    // EAST (side 1)
+    {
+        /* LEFT  */ { 1, 2, 7, -1, -1 },
+        /* STRAIGHT */ { 3, 4, -1, -1, -1 },
+        /* RIGHT */ { 5, -1, -1, -1, -1 },
+        /* UTURN */ { -1, -1, -1, -1, -1 }
+    },
+    // SOUTH (side 2)
+    {
+        /* LEFT  */ { 1, 4, -1, -1, -1 },
+        /* STRAIGHT */ { 2, 3, 5, -1, -1 },
+        /* RIGHT */ { 6, -1, -1, -1, -1 },
+        /* UTURN */ { 7, -1, -1, -1, -1 }  
+    },
+    // WEST (side 3)
+    {
+        /* LEFT  */ { 4, 3, 5, -1, -1 },
+        /* STRAIGHT */ { 1, 2, 6, -1, -1 },
+        /* RIGHT */ { 7, -1, -1, -1, -1 },
+        /* UTURN */ { -1, -1, -1, -1, -1 }
+    }
+};
+
+
+
 static void* manage_light(void* arg)
 {
     int side = ((TrafficLightArgs*)arg)->side;
@@ -76,17 +114,26 @@ static void* manage_light(void* arg)
         // Process the next arrival.
         Car_Arrival arrival = curr_car_arrivals[side][direction][processed_count[side][direction]];
         processed_count[side][direction]++;
+
+        // Retrieve the zone list 
+        int *zones_for_path = path_zones[side][direction];
+        
+        // Lock all zones according to zone list
+        for (int i = 0; zones_for_path[i] != -1; i++) {
+            // Subtract 1 since your zones are 1-indexed.
+            pthread_mutex_lock(&zone_mutex[zones_for_path[i] - 1]);
+        }
             
-        pthread_mutex_lock(&intersection_mutex);
         printf("traffic light %d %d turns green at time %d for car %d\n",
                 side, direction, get_time_passed(), arrival.id);
         sleep(CROSS_TIME);
         printf("traffic light %d %d turns red at time %d\n",
                 side, direction, get_time_passed());
-        pthread_mutex_unlock(&intersection_mutex);
         
-        pthread_mutex_lock(&count_mutex);
-        pthread_mutex_unlock(&count_mutex);
+                // Unlock the zones 
+        for (int i = 0; zones_for_path[i] != -1; i++) {
+            pthread_mutex_unlock(&zone_mutex[zones_for_path[i] - 1]);
+        }
     }
     return (0);
 }
@@ -99,10 +146,17 @@ int main(int argc, char * argv[])
       sem_init(&car_sem[i][j], 0, 0);
     }
   }
-  
+
+
+  // zone mutexes
+  for (int i = 0; i < NUM_ZONES; i++) {
+    pthread_mutex_init(&zone_mutex[i], NULL);
+}
+
+
   start_time(); // This sets begin_time
   
-  // (Optional: initialize end_sem if you want to use it elsewhere)
+
   
   // Create traffic light threads.
   const int lights = 9;  // 9 regular traffic light threads
@@ -138,6 +192,11 @@ int main(int argc, char * argv[])
   for (int i = 0; i < lights + 1; i++) {
       pthread_join(threads[i], NULL);
   }
+
+  // zone mutex destroy
+    for (int i = 0; i < NUM_ZONES; i++) {
+        pthread_mutex_destroy(&zone_mutex[i]);
+    }
   
   // Clean up semaphores.
   for (int i = 0; i < 4; i++) {
